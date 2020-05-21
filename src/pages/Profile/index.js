1abb6d86f3a2aa429cce076271c0ead63b8cea78
Joao Sousa <joao.rsousa23@gmail.com>
2020-05-21 23:30:05 +0930

    adding header, profile page and notifications

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 function Profile() {
-  return <div />;
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onClick={handleSubmit}>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your email adress" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Your actual password"
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Your new password"
+        />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Repeat your new password"
+        />
+
+        <button type="submit"> Update Profile</button>
+      </Form>
+      <button type="button"> Exit from GoBarber</button>
+    </Container>
+  );
 }
 
 export default Profile;
